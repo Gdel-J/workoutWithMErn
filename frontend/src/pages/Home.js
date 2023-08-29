2dce717fd9c57b2ac769b32d384ada41bc80d0c3
Gdel-J <jessygdelvecchio@gmail.com>
2023-08-29 12:18:56 -0400

    WorkoutDeytails.js implemented, WorkoutForm.js created

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+
+//components
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm"
+
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("api/workouts");
+      const json = await response.json();
+
+      if (!response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className="Home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails workout={workout} key={workout._id} />
+          ))}
+      </div>
+      <WorkoutForm/>
     </div>
   );
 };
-export default  Home
+export default Home
