f8a77c9bd67e692b201139e73b7bb7ee843d18b2
Gdel-J <jessygdelvecchio@gmail.com>
2023-08-29 14:39:42 -0400

    WorkoutForm modified

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextProvider } from './context/WorkoutsContext'
-
+import { WorkoutsContextProvider } from './context/WorkoutsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,4 +12,3 @@ root.render(
     </WorkoutsContextProvider>
   </React.StrictMode>
 )
-
